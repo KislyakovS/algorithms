a71a210d4923747c43074d7c5e9b0c3cd78e1ff9
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-12-02 20:23:43 +0300

    append pow

---
+++ algorithms/pow.js
@@ -0,0 +1,10 @@
+// pow - Возведение в степень
+
+const pow = (a, b) => {
+  const binary = b.toString(2);
+
+  return [...binary].reverse().reduce((res, it) => {
+    if (it === '1') res *= a;
+    a *= a;
+  }, 1);
+}
