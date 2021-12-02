09a445ab1d09d5e3c561a5a838fb8dd0716915f8
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-12-02 20:23:30 +0300

    append gcd

---
+++ algorithms/gcd.js
@@ -0,0 +1,16 @@
+// НОД - Наибольшим общим делителем
+
+const gcd_1 = (a, b) => {
+  if (b === 0) return a;
+
+  return gcd_1(b, a % b);
+}
+
+const gcd_2 = (a, b) => {
+  while (b != 0) {
+    if (a > b) a -= b;
+    else b -= a;
+  }
+
+  return a;
+}
