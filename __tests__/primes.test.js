8d06bf00f402758c0d9d844032071e7e0e8a2329
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-12-07 21:45:12 +0300

    add func sieve of eratosthenes

--- __tests__/primes.test.js
+++ __tests__/primes.test.js
@@ -8,4 +8,23 @@ describe('Primes', () => {
   it('should the number 29 be a prime number', () => {
     expect(isPrime(29)).toBe(true);
   });
+
+  it('should get all primes up to 30', () => {
+    const RESULT = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
+    expect(getPrimes(30)).toEqual(RESULT);
+    expect(getPrimes(30)).toHaveLength(RESULT.length);
+  });
+
+  it('should get all primes up to 30', () => {
+    const RESULT = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
+    expect(getPrimes(30)).toEqual(RESULT);
+  });
+
+  it('should get all primes up to 100', () => {
+    const RESULT = [
+      2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
+      43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
+    ];
+    expect(getPrimes(100)).toEqual(RESULT);
+  });
 });
--- src/primes.js
+++ src/primes.js
@@ -16,6 +16,30 @@ const isPrime = (a) => {
   return true;
 }
 
+// Sieve of Eratosthenes - Решето Эратосфена
+
+const getPrimes = (n) => {
+  if (n <= 0) {
+    return [];
+  }
+
+  const sieve = [];
+  const primes = [];
+
+  for (let i = 2; i <= n; i++) {
+    if (!sieve.includes(i)) {
+      primes.push(i);
+
+      for (let j = i * 2; j <= n; j += i) {
+        sieve.push(j);
+      };
+    };
+  };
+
+  return primes;
+}
+
 module.exports = {
   isPrime,
+  getPrimes
 }
