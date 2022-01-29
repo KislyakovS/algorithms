3b79039e8dee5cc2865746c49408f271948e4722
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2022-01-29 17:23:40 +0300

    fix `base` value center list

--- src/quicksort.js
+++ src/quicksort.js
@@ -3,19 +3,19 @@
 const quicksort = (list) => {
   if (list.length <= 1) return list;
 
-  const baseValue = list.pop();
+  const base = list.splice(Math.floor(list.length / 2), 1)[0];
   const prev = [];
   const next = [];
 
   list.forEach(numb => {
-    if (numb > baseValue) {
+    if (numb > base) {
       next.push(numb);
     } else {
       prev.push(numb);
     }
   });
 
-  return [...quicksort(prev), baseValue, ...quicksort(next)];
+  return [...quicksort(prev), base, ...quicksort(next)];
 };
 
 module.exports = { quicksort }
